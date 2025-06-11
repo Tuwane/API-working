e43dfea6e4164bcab7684c662ca823f3a375e7d9
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-06-12 01:21:23 +0200

    adding the varable

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
+let x;
 function checkApiKey() {
     if (!localStorage.getItem("apiKey")) {
         window.open("enter-api-key.html", "_self");
